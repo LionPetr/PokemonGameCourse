3dd8694926754cbe08a62a3ad9c7d9e3bc485b3d
lion <lionpetrenko@gmail.com>
2025-08-26 18:59:19 -0700

    added the introduction and asked for player name

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -2,5 +2,10 @@
 
 int main() {
 
+    std::string player_name;
+    std::cout << "Enter your name" << std::endl;
+    std::cin >> player_name;
+    std::cout << "Great Start " + player_name + ", looks like you have understoof the main() function properly now!" << std::endl;
+
     return 0;
 }
