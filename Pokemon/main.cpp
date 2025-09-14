ca81af82a117d6e096bd82b889189580781c18b6
lion <lionpetrenko@gmail.com>
2025-09-13 18:24:03 -0700

    added skeleton of a game loop with the quitting option implemented

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -2,6 +2,8 @@
 #include <limits>
 
 
+
+
 void waitForEnter()
 {
 	std::cin.get();
@@ -208,6 +210,61 @@ public:
 	}
 };
 
+void gameLoop(Trainer& player)
+{
+	bool keepPlaying = true;
+	int choice;
+
+	while (keepPlaying)
+	{
+		clearConsole();
+		std::cout << "What would you like to do next - " << player.name << std::endl;
+		std::cout << "1. Battle Wild Pokemon" << std::endl;
+		std::cout << "2. Visit PokeCenter" << std::endl;
+		std::cout << "3. Challenge Gyms" << std::endl;
+		std::cout << "4. Enter Pokemon League" << std::endl;
+		std::cout << "5. Quit" << std::endl;
+		std::cout << "Enter your choice: ";
+		std::cin >> choice;
+
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+		switch (choice)
+		{
+		case 1:
+			std::cout << "you suck" << std::endl;
+			break;
+		case 2:
+			std::cout << "wasnt built yet" << std::endl;
+			break;
+		case 3:
+			std::cout << "hit the actual gym first" << std::endl;
+			break;
+		case 4:
+			std::cout << "its out of your league" << std::endl;
+			break;
+		case 5:
+			std::cout << "Are you sure you want to quit?" << std::endl;
+			char quitChoice;
+			std::cin >> quitChoice;
+			if (quitChoice == 'y' || quitChoice == 'Y')
+			{
+				keepPlaying = false;
+			}
+
+			break;
+		default:
+			std::cout << "select one of the choices provided" << std::endl;
+			break;
+
+		}
+		waitForEnter();
+
+
+	}
+	std::cout << "bye bye" << std::endl;
+}
+
 
 
 
@@ -222,10 +279,7 @@ int main()
 	professor.offerPokemonChoices(player);
 	professor.explainMainQuest(player);
 
-
-
-
-	std::cout << "good luck on your journy" << std::endl;
+	gameLoop(player);
 
 	return 0;
 }
